b344eaf0deb92a11af241f2248f781dee2492367
Abdelrhman Saied <abdelrhman.s.farag@gmail.com>
2020-09-01 03:29:58 +0200

    iframe

--- app.js
+++ app.js
@@ -8,6 +8,11 @@ const buttonElement = document.querySelector('#search');
 const inputElement = document.querySelector('#input-value');
 const movieSearchable = document.querySelector('#movies-searchable');
 
+function generateUrl(path) {
+  const url = `https://api.themoviedb.org/3${path}?api_key=21bc84afd76a48f2c4e4a7883c54930f`;
+  return url;
+}
+
 function movieSection(movies) {
   return movies.map((movie) => {
     if (movie.poster_path) {
@@ -26,7 +31,7 @@ function createMovieContainer(movies) {
     <section class="section">
       ${movieSection(movies)}
     </section>
-  <div class="content content-display">
+  <div class="content ">
     <p id="content-close">x</p>
   </div>
   `;
@@ -46,8 +51,8 @@ function renderSearchMovies(data) {
 buttonElement.onclick = (event) => {
   event.preventDefault(); //disable website behavior
   const value = inputElement.value;
-
-  const newUrl = url + '&query=' + value;
+  const path = '/search/movie';
+  const newUrl = generateUrl(path) + '&query=' + value;
   fetch(newUrl)
     .then((res) => res.json())
     .then(renderSearchMovies)
@@ -59,10 +64,52 @@ buttonElement.onclick = (event) => {
   console.log('value:', value);
 };
 
+function createIframe(video) {
+  const iframe = document.createElement('iframe')
+  iframe.src = `https;//youtube.com/embed/${video.key}`;
+  iframe.width = 360;
+  iframe.height = 315;
+  iframe.allowFullscreen = true;
+
+  return iframe
+}
+
+
 document.onclick = (event) => {
   const target = event.target;
 
   if (target.tagName.toLowerCase() === 'img') {
-    console.log('Hello World');
+    const movieId = target.dataset.movieId;
+    console.log(movieId);
+    const section = event.target.parentElement;
+    const content = section.nextElementSibling;
+    content.classList.add('content-display');
+    const path = `/movie/${movieId}/videos`;
+    const url = generateUrl(path);
+
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('videos', data);
+
+        const videos = data.results;
+        const length = videos.length > 4 ? 4 : videos.length;
+        const iframeContainer = document.createElement('div');
+        for (let i = 0; i < videos.length; i++){
+          const video = videos[i];
+          const iframe = createIframe(video);
+          iframeContainer.appendChild(iframe)
+        
+        }
+
+      })
+      .catch((error) => {
+        console.log('error:', error);
+      });
+  }
+
+  if (target.id === 'content-close') {
+    const content = target.parentElement;
+    content.classList.remove('content-display');
   }
 };
